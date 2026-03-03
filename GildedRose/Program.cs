767a665e7dce6ef96cca45c2bab03242e4259c10
Téva JACQUET <PTJT04221@COMMUN.AD.SNCF.FR>
2026-03-03 20:06:18 +0100

    Correctif fichier ThirtyDays

--- GildedRose/Program.cs
+++ GildedRose/Program.cs
@@ -35,7 +35,7 @@ public class Program
                 Quality = 49
             },
             // this conjured item does not work properly yet
-            new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+            new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 13}
         };
 
         var app = new GildedRose(items);
